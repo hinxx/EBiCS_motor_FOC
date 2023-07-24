3994430c1fb1bc8e5461ac747929ba991ca9a2da
Hinko Kocevar <hinkocevar@gmail.com>
2023-07-24 20:18:07 +0200

    codium build stuff

--- example/build.sh
+++ example/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export GCC_PATH=/data/data/ecoride/gcc-arm-none-eabi-9-2020-q2-update/bin
+export GCC_PATH=/home/hinxx/Projects/ecoride/EBiCS/gcc-arm-none-eabi-9-2020-q2-update/bin
 make $1
